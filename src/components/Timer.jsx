db76fd0b1db40c2aa6cf724b3fa251ad2d3e78fc
TrinhCongHuy <trinhconghuy.2003@gmail.com>
2024-08-25 22:39:01 +0700

    update timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,23 +1,24 @@
 import React, { useEffect } from 'react';
 
-const Timer = ({ time, setTime, isGameOver, isGameStarted }) => {
+const Timer = ({ time, setTime, isGameOver, isGameStarted, isGameCleared }) => {
   useEffect(() => {
     let timer;
-    if (isGameStarted && !isGameOver) {
+
+    if (isGameStarted && !isGameOver && !isGameCleared) {
       timer = setInterval(() => {
-        setTime((prevTime) => prevTime + 0.1);
+        setTime((prevTime) => prevTime + 0.1); 
       }, 100);
-    } else if (isGameOver) {
-      clearInterval(timer);
     }
+
     return () => clearInterval(timer);
-  }, [isGameStarted, isGameOver, setTime]);
+  }, [isGameStarted, isGameOver, isGameCleared, setTime]);
 
   return (
     <div className="timer">
-      Time: <span>{time.toFixed(1)}s</span>
+      Time: <span>{time.toFixed(1)}s</span> 
     </div>
   );
 };
+
 
 export default Timer;
